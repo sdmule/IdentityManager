27707a5af83bf2eed0fde71fa4151209db1bb0dc
sdmule <mulesaurabh45@gmail.com>
2026-06-30 14:02:06 +0530

    Added IdentityDbContext to project by installing NuGet packages

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+    }
+}
